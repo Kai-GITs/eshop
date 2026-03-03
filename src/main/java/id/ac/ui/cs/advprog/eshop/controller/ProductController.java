40cfc5372ee192ad5f3abc0785ecd6bd93ddf0b4
Kai <klfn45@gmail.com>
2026-03-03 17:19:38 +0700

    add home route redirect to product list

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -11,6 +11,14 @@ import java.util.List;
 import java.util.UUID;
 
 @Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/product/list";
+    }
+}
+
+@Controller
 @RequestMapping("/product")
 public class ProductController {
 
